04cef91811256906543eb2777f9c6319db43b25d
jjone <jiwon.j306@gmail.com>
2019-02-27 15:39:29 +0900

    ~ section 6 add

--- 00. cv2_basics.py
+++ 00. cv2_basics.py
@@ -3,6 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+# Importing the images
+def load_img(filename):
+    img = cv2.imread(filename).astype(np.float32) / 255
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img 
+
+# Canvas setting
+def display_img(img):
+    fig = plt.figure(figsize=(12,10))
+    ax = fig.add_subplot(111)
+    ax.imshow(img,cmap='gray')
+    
+
 from PIL import Image
 img = Image.open('img.jpg')
 img_arr = np.asarray(img)
@@ -10,19 +23,6 @@ img_arr.shape
 plt.imshow(img_arr)
 
 plt.imshow(img_arr[:, :, 0], cmap = 'gray')
-
-# Importing the images
-def load_img(filename):
-    img = cv2.imread(filename).astype(np.float32) / 255
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return img 
-
-
-# Canvas setting
-def show_img(img, cmap = None):
-    fig = plt.figure(figsize = (15, 15))
-    ax = fig.add_subplot(111)
-    ax.imshow(img, cmap = cmap)
 
 #################################################
 img = cv2.imread('img.jpg')
@@ -46,7 +46,7 @@ plt.imshow(img_2)
 # saving
 cv2.imwrite('new_image.jpg', img)
 
-########## Drawing on images
+########## Drawing 
 blank_img = np.zeros(shape = (512, 512, 3), dtype = np.int16)
 plt.imshow(blank_img)
 
@@ -75,7 +75,8 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(blank_img, text = 'Hello', org = (10, 500), fontFace = font, fontScale = 4,
             color = (255, 255, 255), thickness = 3, lineType = cv2.LINE_AA)
 
-########## Blending 
+
+############################## Blending 
 # blending with the same size
 blended = cv2.addWeighted(src1 = img_1, alpha = .5, src2 = img_2, beta = .5, gamma = 10)
 
@@ -90,13 +91,15 @@ y_end = y_start + small_img.shape[0]
 
 large_img[y_start:y_end, x_start:x_end] = small_img
 
-########## Image Threshold
+
+############################## Threshold
 ret, thresh_1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)  # THRESH_BINARY_INV, THRESH_TRUNC
 plt.imshow(thresh_1, cmap = 'gray')
 
 thresh_2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 8)
 
-########## Bluring & Smoothing
+
+############################## Bluring & Smoothing
 # gamma correction 
 img_2 = np.power(img, gamma)
 show_img(img_2)
@@ -106,6 +109,47 @@ kernel = np.ones(shape = (5, 5), dtype = np.float32) / 25
 img_2 = cv2.filter2D(img, -1, kernel)
 
 # cv2 builtin 
-img_2 = cv2.blur(img, ksize = (5, 5))  
-img_2 = cv2.GaussianBlur(img, ksize = (5, 5), 10)   
-img_2 = cv2.medianBlur(img, 5)    
+cv2.blur(img, ksize = (5, 5))  
+cv2.GaussianBlur(img, ksize = (5, 5), 10)   
+cv2.medianBlur(img, 5)    
+cv2.bilateralfilter(img, 9, 75, 75)
+
+
+############################## Morphological Operator 
+# erosion (eroding away the boundary)
+kernel = np.ones((5, 5), np.uint8)
+img_2 = cv2.erode(img, kernel, iterations = 3)
+
+# opening (removing background noise)
+img_2 = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+# closing (removing foreground noise)
+img_2 = cv2.morphologyEx(black_noise_img, cv2.MORPH_CLOSE, kernel)
+
+# diff between dilation & erosion
+img_2 = cv2.morphologyEx(img,cv2.MORPH_GRADIENT,kernel)
+
+
+############################## Gradients
+sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = 5)
+sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = 5)
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+
+
+############################## Color Histogram
+hist_values = cv2.calcHist([img], channels=[0], mask=None, histSize=[256], ranges=[0,256])
+plt.plot(hist_values)
+
+color = ('b','g','r')
+for i, col in enumerate(color):
+    histr = cv2.calcHist([img], [i], None, [256], [0,256])
+    plt.plot(histr, color = col)
+    plt.xlim([0,256])
+plt.show()
+
+
+############################## Histogram Equalization
+img_2 = cv2.equalizeHist(img)
+
+
+
